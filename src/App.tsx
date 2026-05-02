5b87774ac1853b8780b58f94008c65dca8122316
workingpoormann <working.poor.system.programming@gmail.com>
2026-05-02 14:42:28 +0900

    default vite@latest tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Problems } from "./Problems";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1 className="text-xl">hello world</h1>
+      </header>
+
+      <Problems />
+    </>
+  );
+}
+
+export default App;
