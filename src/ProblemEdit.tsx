5cc04b454ca52e84ad6675e9eb7867e2da8b0462
workingpoormann <working.poor.system.programming@gmail.com>
2026-05-03 02:41:14 +0900

    frontend: problem edit

---
+++ src/ProblemEdit.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { NavLink, useNavigate, useParams } from "react-router";
+import type { ProblemType } from "../types/problem";
+
+const fetchData = async (id: number) => {
+  const res = await fetch(`http://localhost:3001/problem/${id}`);
+  const data = await res.json();
+  return data;
+};
+
+export default function ProblemEdit() {
+  const navigate = useNavigate();
+  const { problemId } = useParams();
+
+  const [problem, setProblem] = React.useState<ProblemType>({
+    id: 0,
+    title: "",
+    question: "",
+    answer: "",
+  });
+
+  React.useEffect(() => {
+    fetchData(Number(problemId)).then(setProblem);
+  }, [problemId]);
+
+  return (
+    <section>
+      <form
+        method="put"
+        className="flex flex-col gap-2 mx-10"
+        onSubmit={async (e) => {
+          e.preventDefault();
+
+          const formData = new FormData(e.currentTarget);
+          await fetch(`http://localhost:3001/problem/edit/${problemId}`, {
+            method: "PUT",
+            body: JSON.stringify({
+              title: formData.get("title"),
+              question: formData.get("question"),
+              answer: formData.get("answer"),
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+
+          navigate("/");
+        }}
+      >
+        <header className="flex justify-around items-baseline">
+          <NavLink to="/">
+            <h1 className="text-xl">Problem Edit</h1>
+          </NavLink>
+
+          <button
+            type="submit"
+            className={`border-2 text-xl px-2 rounded-lg 
+              border-amber-900 hover:border-amber-200
+              bg-amber-900 hover:bg-amber-200
+              text-amber-50 hover:text-amber-900
+              transition delay-50`}
+          >
+            Edit
+          </button>
+        </header>
+
+        <section className="flex flex-col gap-6">
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Title"
+            defaultValue={problem.title}
+            className={`border border-gray-400 rounded-md p-2
+              text-lg text-gray-200`}
+          />
+
+          <textarea
+            name="question"
+            id="question"
+            placeholder="Question"
+            defaultValue={problem.question}
+            className={`border border-gray-400 rounded-md p-2 h-64
+              text-lg text-gray-200`}
+          ></textarea>
+
+          <textarea
+            name="answer"
+            id="answer"
+            placeholder="Answer"
+            defaultValue={problem.answer}
+            className={`border border-gray-400 rounded-md p-2 h-64
+              text-lg text-gray-200`}
+          ></textarea>
+        </section>
+      </form>
+    </section>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,18 @@ import App from "./App.tsx";
 import "./index.css";
 import ProblemCreate from "./ProblemCreate.tsx";
 import ProblemDetail from "./ProblemDetail.tsx";
+import ProblemEdit from "./ProblemEdit.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/problem/create" element={<ProblemCreate />} />
-        <Route path="/problem/:problemId" element={<ProblemDetail />} />
+        <Route path="/problem">
+          <Route path="create" element={<ProblemCreate />} />
+          <Route path=":problemId" element={<ProblemDetail />} />
+          <Route path="edit/:problemId" element={<ProblemEdit />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
