aa8ee96bd573cd13147e5c54d3d3759f3a736b17
workingpoormann <working.poor.system.programming@gmail.com>
2026-05-02 19:07:33 +0900

    attach react-router and Link to each a Problem

--- src/Problems.tsx
+++ src/Problems.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import type { ProblemType } from "../types/problem";
+import { Link } from "react-router";
 
 const fetchData = async () => {
-  const res = await fetch("http://localhost:3001/");
+  const res = await fetch("http://localhost:3001/problem");
   const data = await res.json();
   return data;
 };
@@ -12,20 +13,19 @@ export default function Problems() {
 
   React.useEffect(() => {
     fetchData().then((data) => {
-      console.log(data);
       setProblems(data);
     });
   }, []);
 
   return (
-    <section>
-      <div>
-        {problems.map((problem) => (
-          <p key={problem.id}>
+    <section className="flex flex-col mx-20 gap-10">
+      {problems.map((problem) => (
+        <Link key={problem.id} to={`/problem/${problem.id}`}>
+          <div className="border border-solid p-2">
             {problem.question} {problem.answer}
-          </p>
-        ))}
-      </div>
+          </div>
+        </Link>
+      ))}
     </section>
   );
 }
