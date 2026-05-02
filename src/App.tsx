703b50cb804f0654b82b0f1ee8b41957f58b554b
workingpoormann <working.poor.system.programming@gmail.com>
2026-05-02 19:29:22 +0900

    add Link to Problem Create

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import Problems from "./Problems";
 export default function App() {
   return (
     <>
-      <header>
+      <header className="flex gap-10 items-baseline px-40">
         <NavLink to="/">
           <h1 className="text-xl">Simple Quiz</h1>
+        </NavLink>
+
+        <NavLink
+          to="/problem/create"
+          className={`bg-sky-900 text-sky-50 rounded-sm px-2`}
+        >
+          Create
         </NavLink>
       </header>
 
